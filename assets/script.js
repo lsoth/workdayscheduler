4997b523b1bd581d451b91bcd977eb2ac1d2cfb8
Mamba <84301026+lsoth@users.noreply.github.com>
2021-07-01 19:50:44 -0700

    changed formatting of html so timeblocks are divs instead of tables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+//Displays time on top of page
 var time = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(time);
 console.log(time)
+
+//Used to save text content of adjenda even after refresh using local storeage
+var form1 = document.querySelector("#form1")
